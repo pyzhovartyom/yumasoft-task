5591166f2795643c6e6144c15ad9fa4102f6417d
Artyom Pyzhov <pyzhovartyom@gmail.com>
2021-05-02 14:54:46 +0300

    feat: change textarea styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TextareaComponent } from './components/textarea/textarea.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TextareaComponent
+    TextareaComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
